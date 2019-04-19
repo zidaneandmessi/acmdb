7080224b7be473580ce94eec56c69a0ca1d4aff7
zidaneandmessi <dimar@qq.com>
2019-04-19 21:55:43 +0800

    init

---
+++ src/java/simpledb/Tuple.java
@@ -0,0 +1,148 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Tuple maintains information about the contents of a tuple. Tuples have a
+ * specified schema specified by a TupleDesc object and contain Field objects
+ * with the data for each field.
+ */
+public class Tuple implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private TupleDesc tupleDesc;
+    private RecordId recordId;
+    private Field[] fieldAr;
+
+    /**
+     * Create a new tuple with the specified schema (type).
+     *
+     * @param td
+     *            the schema of this tuple. It must be a valid TupleDesc
+     *            instance with at least one field.
+     */
+    public Tuple(TupleDesc td) {
+        // some code goes here
+    	this.tupleDesc = td;
+    	this.fieldAr = new Field[td.numFields()];
+    }
+
+    /**
+     * @return The TupleDesc representing the schema of this tuple.
+     */
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+        return tupleDesc;
+    }
+
+    /**
+     * @return The RecordId representing the location of this tuple on disk. May
+     *         be null.
+     */
+    public RecordId getRecordId() {
+        // some code goes here
+        return recordId;
+    }
+
+    /**
+     * Set the RecordId information for this tuple.
+     *
+     * @param rid
+     *            the new RecordId for this tuple.
+     */
+    public void setRecordId(RecordId rid) {
+        // some code goes here
+    	this.recordId = rid;
+    }
+
+    /**
+     * Change the value of the ith field of this tuple.
+     *
+     * @param i
+     *            index of the field to change. It must be a valid index.
+     * @param f
+     *            new value for the field.
+     */
+    public void setField(int i, Field f) {
+        // some code goes here
+    	fieldAr[i] = f;
+    }
+
+    /**
+     * @return the value of the ith field, or null if it has not been set.
+     *
+     * @param i
+     *            field index to return. Must be a valid index.
+     */
+    public Field getField(int i) {
+        // some code goes here
+        if (i < 0 || i >= tupleDesc.numFields())
+            return null;
+        return fieldAr[i];
+    }
+
+    /**
+     * Returns the contents of this Tuple as a string. Note that to pass the
+     * system tests, the format needs to be as follows:
+     *
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     *
+     * where \t is any whitespace (except a newline)
+     */
+    public String toString() {
+        // some code goes here
+        String S = "";
+        for (int i = 0; i < fieldAr.length - 1; i++)
+        	S += fieldAr[i].toString() + "\t";
+        if (fieldAr.length != 0)
+        	S += fieldAr[fieldAr.length - 1].toString();
+        return S;
+    }
+
+    public class ArrayIterator<E> implements Iterator<E> {
+
+            public E[] obj;
+            public int len;
+            public int index;
+
+            public ArrayIterator(E[] a) {
+                this.obj = a;
+                this.len = a.length;
+                this.index = 0;
+            }
+
+            public boolean hasNext() {
+                return index < len && obj[index] != null;
+            }
+
+            public E next() {
+                return obj[index++];
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        }
+
+    /**
+     * @return
+     *        An iterator which iterates over all the fields of this tuple
+     * */
+    public Iterator<Field> fields()
+    {
+        // some code goes here
+        return new ArrayIterator<Field>(fieldAr);
+    }
+
+    /**
+     * reset the TupleDesc of thi tuple
+     * */
+    public void resetTupleDesc(TupleDesc td)
+    {
+        // some code goes here
+    	this.tupleDesc = td;
+    	this.fieldAr = new Field[td.numFields()];
+    }
+}
